c10af4310d226ae1b2ba813073b5f81d68218cb1
Luis Uchoa <luis.nascimento47@etec.sp.gov.br>
2020-06-15 06:32:22 -0300

    first commit

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro eh ${vel}km/h`)
+if (vel > 60) { //condicao simples
+    console.log(`Voce ultrapassou a velocidade permitida, multado!`)
+}
+console.log(`Dirija sempre usando cinto de seguranca!`)
